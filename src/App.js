ba8fabcaf6e38fa1e8be49590b82f11d34c2c390
Ming Chai <ming.chai4@gmail.com>
2019-05-05 10:40:14 -0700

    convert App to class component; add fetch request; clean up notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [], filteredProducts: [] };
+  }
+
+  componentWillMount() {
+    fetch("http://localhost:9000/products")
+      .then(res => res.json)
+      .then(data =>
+        this.setState({
+          products: data,
+          filteredProducts: data
+        })
+      );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>eCommerce Sample App</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-8">
+            <Products
+              products={this.state.filteredProducts}
+              handleAddToCart={this.handleAddToCart}
+            />
+          </div>
+          <div className="col-md-4" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
