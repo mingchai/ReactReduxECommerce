64bdd2544615cc239ed6a5cd029058e1ed930240
Ming Chai <ming.chai4@gmail.com>
2019-05-05 11:32:57 -0700

    create Product component with layout; add to cart function pending

---
+++ src/components/Products.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import util from '../util';
+
+export class Products extends Component {
+  render() {
+    const productsItems = this.props.products.map(product => (
+      <div className="col-md-4">
+        <div className="thumbnail text-center">
+          <a href={`#${product.id}`} onClick={this.props.handleAddToCart}>
+            <img src={`/products/${product.sku}_2.jpg`} alt={product.title} />
+            <p>{product.title}</p>
+          </a>
+          <div>
+            <b>{util.formatCurrency(product.price)}</b>
+            <button className="btn btn-primary" onClick={(e) => this.props.handleAddToCart(e, product)}> Add to Cart </button>
+          </div>
+        </div>
+      </div>
+    ));
+    return <div className="row">{productsItems}</div>;
+  }
+}
+
+export default Products;
