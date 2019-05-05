3c67fc4d1e4b754b86fcda2eba230e0a1393e0de
Ming Chai <ming.chai4@gmail.com>
2019-05-05 13:04:46 -0700

    add filter by price, with notes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { products: [], filteredProducts: [] };
+    this.handleChangeSort = this.handleChangeSort.bind(this);
   }
 
   componentWillMount() {
@@ -20,6 +21,35 @@ class App extends React.Component {
       );
   }
 
+  handleChangeSort(e) {
+    this.setState({ sort: e.target.value });
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.setState(state => {
+      if (state.sort !== "") {
+        state.products.sort((a, b) => (
+          // use a callback function to determine sorting criteria
+          (state.sort === "lowest")
+          // did the user select the option with value of 'lowest' (i.e. sort from lowest to highest)? If not, then they've decided to sort from highes to lowest
+            ? (a.price > b.price
+              // if element 'a' has a higher price, 'b' comes first, else 'a' comes first
+              // if both have the same price, leave the order unchanged
+              ? 1
+              : -1)
+            : (a.price < b.price
+              // similar logic as above, if element 'b' has a higher price, it gets indexed to a higher position than element 'a'
+            ? 1
+            : -1)
+        )
+        )} else {
+        state.products.sort((a, b) => (a.id > b.id ? 1 : -1));
+      }
+      return { filteredProducts: state.products };
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -27,14 +57,14 @@ class App extends React.Component {
         <hr />
         <div className="row">
           <div className="col-md-8">
-            <Filter 
-              size={this.state.size} 
-              sort={this.state.sort} 
-              handleChangeSize={this.handleChangeSize} 
-              handleChangeSort={this.handleChangeSort} 
+            <Filter
+              size={this.state.size}
+              sort={this.state.sort}
+              handleChangeSize={this.handleChangeSize}
+              handleChangeSort={this.handleChangeSort}
               count={this.state.filteredProducts.length}
             />
-            <hr/>
+            <hr />
             <Products
               products={this.state.filteredProducts}
               handleAddToCart={this.handleAddToCart}
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,7 +4,33 @@ export class Filter extends Component {
   render() {
     return (
       <div className="row">
-        Test
+        <div className="col-md-4">
+          {this.props.count} products found
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="">
+            Order By
+            <select className="form-control" value={this.props.sort} onChange={this.props.handleChangeSort} >
+              <option value="">Select</option>
+              <option value="lowest">Lowest to Highest</option>
+              <option value="highest">Highest to Lowest</option>
+            </select>
+          </label>
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="">
+            Order by Size
+            <select className="form-control" value={this.props.size} onChange={this.props.handleChangeSize} >
+              <option value="">All</option>
+              <option value="x">X-Small</option>
+              <option value="s">Small</option>
+              <option value="m">Medium</option>
+              <option value="l">Large</option>
+              <option value="xl">X-Large</option>
+              <option value="xxl">XX-Large</option>
+            </select>
+          </label>
+        </div>
       </div>
     )
   }
